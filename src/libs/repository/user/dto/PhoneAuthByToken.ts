cef67b1ee9f53160f25a407170b802dc62e57dd9
sceonn <yoou201@naver.com>
2023-11-06 22:54:40 +0900

    feat: create phoneAuthByToken

---
+++ src/libs/repository/user/dto/PhoneAuthByToken.ts
@@ -0,0 +1,30 @@
+import { DateTimeUtil } from '../../../util/DateTimeUtil';
+
+export class PhoneAuthByToken {
+  private readonly expiredMinute = 5;
+
+  private id: number;
+
+  private certified: boolean;
+
+  private ttl: Date;
+
+  constructor(ctx: { id: number; certified: boolean; ttl: Date }) {
+    this.id = ctx.id;
+    this.certified = ctx.certified;
+    this.ttl = ctx.ttl;
+  }
+
+  certify(now: Date) {
+    this.certified = true;
+    this.ttl = DateTimeUtil.DateAddMinute(now, this.expiredMinute);
+  }
+
+  isExpired(now: Date) {
+    return now > this.ttl;
+  }
+
+  isCertified(now: Date) {
+    return this.certified && !this.isExpired(now);
+  }
+}

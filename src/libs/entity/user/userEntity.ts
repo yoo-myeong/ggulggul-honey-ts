17aff61b552e10659dbf54f5f9f8b9105d05052c
sceonn <yoou201@naver.com>
2023-11-06 23:20:01 +0900

    feat: create userEntity

---
+++ src/libs/entity/user/userEntity.ts
@@ -0,0 +1,45 @@
+import { UserStatus } from './enum/userStatus';
+import { BaseTimeEntity } from '../BaseTimeEntity';
+
+export class UserEntity extends BaseTimeEntity {
+  id: number;
+
+  email: string;
+
+  password: string;
+
+  firstName: string;
+
+  lastName: string;
+
+  status: UserStatus;
+
+  nickname: string;
+
+  phone: string;
+
+  role: UserRole;
+
+  static signUp(ctx: {
+    email: string;
+    password: string;
+    firstName: string;
+    lastName: string;
+    status: UserStatus;
+    nickname: string;
+    phone: string;
+    role: UserRole;
+  }) {
+    const user = new UserEntity();
+    user.email = ctx.email;
+    user.password = ctx.password;
+    user.firstName = ctx.firstName;
+    user.lastName = ctx.lastName;
+    user.status = ctx.status;
+    user.nickname = ctx.nickname;
+    user.phone = ctx.phone;
+    user.role = ctx.role ?? UserRole.BUYER;
+
+    return user;
+  }
+}

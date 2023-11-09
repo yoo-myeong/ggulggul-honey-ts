20d6c88402d6a39dc757384dca25e35b81aba6a6
sceonn <yoou201@naver.com>
2023-11-09 23:34:51 +0900

    feat: add Of

--- src/libs/util/Of.ts
+++ src/libs/util/Of.ts
@@ -2,7 +2,7 @@ import { ClassConstructor, plainToInstance } from 'class-transformer';
 import { validateSync } from 'class-validator';
 
 export function Of<T extends object, V>(plain: ClassConstructor<T>, instance: V) {
-  const result = plainToInstance<T, V>(plain, instance);
+  const result = plainToInstance<T, V>(plain, instance, { excludeExtraneousValues: true });
   validateSync(result);
 
   return result;

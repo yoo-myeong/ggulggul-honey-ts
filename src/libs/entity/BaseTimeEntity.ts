ae685d163154f8e4559fc1517ff5e9e972b24afc
sceonn <yoou201@naver.com>
2023-11-09 23:34:32 +0900

    feat: BaseTimeEntity decorator

--- src/libs/entity/BaseTimeEntity.ts
+++ src/libs/entity/BaseTimeEntity.ts
@@ -1,7 +1,12 @@
+import { Column, CreateDateColumn, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+
 export class BaseTimeEntity {
+  @PrimaryGeneratedColumn()
   id: number;
 
+  @CreateDateColumn({ type: 'timestamptz' })
   createdAt: Date;
 
+  @UpdateDateColumn({ type: 'timestamptz' })
   modifiedAt: Date;
 }

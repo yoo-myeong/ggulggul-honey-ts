1597acfb479eaf69b02710d1762921ccb1e80cfe
sceonn <yoou201@naver.com>
2023-12-27 22:45:58 +0900

    feat: error handler

---
+++ src/libs/error/errorHandler.ts
@@ -0,0 +1,19 @@
+import { ErrorRequestHandler } from 'express';
+import { CustomError } from './CustomError';
+
+export const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  if (CustomError.isCustomError(err)) {
+    res.status(err.statusCode);
+    if (process.env.NODE_ENV !== 'prod') {
+      res.json({
+        message: err.message,
+      });
+    }
+
+    return res;
+  }
+
+  return res.status(500).json({
+    message: 'internal server error',
+  });
+};

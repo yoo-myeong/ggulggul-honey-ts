13ee12702626741978074c01c9f42cbd365cbcdf
sceonn <yoou201@naver.com>
2023-12-28 21:14:21 +0900

    chore: eslint error ignore

--- src/api/filter/errorHandler.ts
+++ src/api/filter/errorHandler.ts
@@ -1,6 +1,7 @@
 import { ErrorRequestHandler } from 'express';
 import { CustomError } from './CustomError';
 
+// eslint-disable-next-line
 export const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
   if (CustomError.isCustomError(err)) {
     res.status(err.statusCode);

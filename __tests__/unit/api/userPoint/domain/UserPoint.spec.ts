a8a47f483994ba3bcf068387e9cb5ef907b7b5a7
sceonn <yoou201@naver.com>
2023-12-28 23:54:19 +0900

    feat: UserPoint 도메인 포인트 사용 제약

---
+++ __tests__/unit/api/userPoint/domain/UserPoint.spec.ts
@@ -0,0 +1,31 @@
+import { UserPointDomain } from '../../../../../src/api/userPoint/domain/UserPoint';
+import { UserPointDomainUseParams } from '../../../../../src/api/userPoint/domain/dto/UserPointDomainUseParams';
+import { CustomError } from '../../../../../src/api/filter/CustomError';
+
+describe('UserPointDomain', () => {
+  it('보유한 포인트보다 큰 포인트를 사용할 수 없다', async () => {
+    const point = 1000;
+    const sut = await UserPointDomain.from({
+      userPointId: 1,
+      point,
+    });
+    const param = new UserPointDomainUseParams();
+    param.changePoint = 1500;
+    param.modifiedBy = 'user';
+
+    expect(() => sut.use(param)).toThrow(CustomError);
+  });
+
+  it('사용하려는 포인트가 사용가능 최대 포인트(2000)을 넘으면 에러를 던진다', async () => {
+    const point = 3000;
+    const sut = await UserPointDomain.from({
+      userPointId: 1,
+      point,
+    });
+    const param = new UserPointDomainUseParams();
+    param.changePoint = 2500;
+    param.modifiedBy = 'user';
+
+    expect(() => sut.use(param)).toThrow(CustomError);
+  });
+});
--- src/api/userPoint/domain/UserPoint.ts
+++ src/api/userPoint/domain/UserPoint.ts
@@ -1,30 +1,35 @@
 import { UserPointDomainUseParams } from './dto/UserPointDomainUseParams';
+import { of } from '../../../libs/util/of';
+import { CustomError } from '../../filter/CustomError';
+import { ErrorCode } from '../../../libs/error/errorCode';
 
 export class UserPointDomain {
   private readonly MAX_USABLE_POINT = 2000;
+  private readonly _userPointId: number;
+  private _point: number;
+  private _changePoint?: number;
+  private _modifiedBy?: string;
 
-  private readonly userPointId: number;
+  static async from(params: { userPointId: number; point: number }) {
+    return of(this, params);
+  }
 
-  private point: number;
-
-  private changePoint?: number;
-
-  private modifiedBy?: string;
-
-  private setPointBy(changePoint: number) {
-    const remainPoint = this.point + changePoint;
+  private setPointBy(changePoint: number, modifiedBy: string) {
+    const remainPoint = this._point + changePoint;
     if (remainPoint < 0) {
-      throw new Error('가능한 포인트 사용량이 아닙니다.');
+      throw new CustomError(ErrorCode.BAD_REQUEST, '포인트는 음수가 될 수 없습니다.');
     }
-    this.point += changePoint;
+    this._point += changePoint;
+    this._changePoint = changePoint;
+    this._modifiedBy = modifiedBy;
   }
 
   private validateChangePoint(changePoint: number) {
     const absChangePoint = Math.abs(changePoint);
-    const isUsable = changePoint < 0 && absChangePoint <= this.MAX_USABLE_POINT && absChangePoint <= this.point;
+    const isUsable = changePoint < 0 && absChangePoint <= this.MAX_USABLE_POINT && absChangePoint <= this._point;
 
     if (!isUsable) {
-      throw new Error('포인트를 사용할 수 없습니다.');
+      throw new CustomError(ErrorCode.BAD_REQUEST, '포인트를 사용할 수 없습니다.');
     }
   }
 
@@ -32,7 +37,6 @@ export class UserPointDomain {
     const { changePoint, modifiedBy } = params;
     this.validateChangePoint(changePoint);
 
-    this.setPointBy(changePoint);
-    this.modifiedBy = modifiedBy;
+    this.setPointBy(changePoint, modifiedBy);
   }
 }

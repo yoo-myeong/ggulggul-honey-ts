b0a56d30b2726e00069604a677d5da71e2e72191
sceonn <yoou201@naver.com>
2024-01-06 16:29:38 +0900

    feat: typeorm config

--- src/api/config/configContainer.ts
+++ src/api/config/configContainer.ts
@@ -1,3 +1,4 @@
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 import { Config } from './config';
 
 const APP_PORT = Config.cast('APP_PORT').getParsedInt();
@@ -5,13 +6,22 @@ export const APP = {
   APP_PORT,
 };
 
-const GOOGLE_CLIENT_ID = Config.cast('GOOGLE_CLIENT_ID').getString();
-const GOOGLE_CLIENT_SECRET = Config.cast('GOOGLE_CLIENT_SECRET').getString();
-const GOOGLE_REDIRECT_URI = Config.cast('GOOGLE_REDIRECT_URI').getString();
-const GOOGLE_TOKEN_URL = Config.cast('GOOGLE_TOKEN_URL').getString();
 export const GOOGLE = {
-  GOOGLE_CLIENT_ID,
-  GOOGLE_CLIENT_SECRET,
-  GOOGLE_REDIRECT_URI,
-  GOOGLE_TOKEN_URL,
+  GOOGLE_CLIENT_ID: Config.cast('GOOGLE_CLIENT_ID').getString(),
+  GOOGLE_CLIENT_SECRET: Config.cast('GOOGLE_CLIENT_SECRET').getString(),
+  GOOGLE_REDIRECT_URI: Config.cast('GOOGLE_REDIRECT_URI').getString(),
+  GOOGLE_TOKEN_URL: Config.cast('GOOGLE_TOKEN_URL').getString(),
+};
+
+export const MYSQL = {
+  type: 'mysql',
+  host: Config.cast('MYSQL_HOST').getString(),
+  port: Config.cast('MYSQL_PORT').getParsedInt(),
+  username: Config.cast('MYSQL_USERNAME').getString(),
+  password: Config.cast('MYSQL_PASSWORD').getString(),
+  database: Config.cast('MYSQL_DATABASE').getString(),
+  autoLoadEntities: true,
+  synchronize: !!(Config.cast('NODE_ENV').getString() === 'local' || 'test'),
+  logging: !!(Config.cast('NODE_ENV').getString() === 'local' || 'test'),
+  namingStrategy: new SnakeNamingStrategy(),
 };

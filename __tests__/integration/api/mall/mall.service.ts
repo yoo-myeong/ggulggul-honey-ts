2818edb47e7098da0e1231c23118a54f1472ecdf
sceonn <yoou201@naver.com>
2024-01-07 18:55:40 +0900

    test: id를 통해 매장 정보를 조회한다

---
+++ __tests__/integration/api/mall/mall.service.ts
@@ -0,0 +1,25 @@
+import { Repository } from 'typeorm';
+import { TypeOrm } from '../../../../src/libs/repository/TypeOrm';
+import { MallEntity } from '../../../../src/libs/entity/mall/MallEntity';
+
+describe('MallService', () => {
+  let mallRepository: Repository<MallEntity>;
+
+  beforeEach(async () => {
+    await TypeOrm.connect(getMySqlTypeOrmTestOption());
+    mallRepository = TypeOrm.getRepository(MallEntity);
+  });
+
+  it('id를 통해 매장 정보를 조회한다', () => {
+    const mallCustomRepo = new MallCustomRepository(mallRepository);
+    const sut = new MallService(mallCustomRepo);
+    const mall = new MallEntity();
+    mall.address = '서울 강남구 대치동';
+    mall.phone = '02-123-456';
+
+    const mall = await mallRepository.save(mall);
+    const result = mallCustomRepo.findById(mall.id);
+
+    expect(mall.id).toEqual(result.id);
+  });
+});

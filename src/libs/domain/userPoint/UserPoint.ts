c201749523ab89ea792af267c4afef1b28d0b636
sceonn <yoou201@naver.com>
2024-01-09 22:41:47 +0900

    fix: 포인트 사용 버그 수정

--- src/libs/domain/userPoint/UserPoint.ts
+++ src/libs/domain/userPoint/UserPoint.ts
@@ -4,11 +4,16 @@ import { ErrorCode } from '../../error/errorCode';
 
 export class UserPointDomain {
   private readonly MAX_USABLE_POINT = 2000;
+  private readonly MAX_ADDABLE_POINT = 2000;
   private readonly USE_POINT_DIVISIBLE_BY_VALUE = 500;
+  private readonly ADD_POINT_DIVISIBLE_BY_VALUE = 100;
   private _point: number;
 
-  static async from(params: { userPointId: number; point: number }) {
-    return of(this, params);
+  static async from(params: { point: number }) {
+    const inst = new UserPointDomain();
+    inst._point = params.point;
+
+    return inst;
   }
 
   private setPoint(changePoint: number) {
@@ -32,7 +37,28 @@ export class UserPointDomain {
   }
 
   use(changePoint: number) {
-    this.validateUsingPoint(changePoint);
-    this.setPoint(changePoint);
+    const usingPoint = -changePoint;
+    this.validateUsingPoint(usingPoint);
+    this.setPoint(usingPoint);
+
+    return this._point;
+  }
+
+  private validateAddingPoint(changePoint: number) {
+    const isAddable =
+      changePoint > 0 && changePoint % this.ADD_POINT_DIVISIBLE_BY_VALUE === 0 && changePoint <= this.MAX_ADDABLE_POINT;
+
+    if (!isAddable) {
+      throw new CustomError(ErrorCode.BAD_REQUEST, '충전할 수 없는 포인트입니다.');
+    }
+  }
+
+  add(changePoint: number) {
+    const addingPoint = changePoint;
+
+    this.validateAddingPoint(addingPoint);
+    this.setPoint(addingPoint);
+
+    return this._point;
   }
 }

50895d2bb46379bd5e85995bdbbac02e587da5ae
sceonn <yoou201@naver.com>
2024-01-12 20:04:32 +0900

    fix: 랜덤포인트 지급 버그

--- __tests__/unit/libs/domain/userPoint/RandomPoint.spec.ts
+++ __tests__/unit/libs/domain/userPoint/RandomPoint.spec.ts
@@ -1,19 +1,19 @@
 import { RandomPoint } from '../../../../../src/libs/domain/userPoint/RandomPoint';
 
 describe('RandomPoint', () => {
-  it('첫 포인트 지급은 500~700원 사이이다.', () => {
+  it('첫 포인트 지급은 300~700원 사이이다.', () => {
     const sut = new RandomPoint();
 
     const point = sut.generateRandomPointForFirst();
 
-    expect(point >= 500).toBeTruthy();
+    expect(point >= 300).toBeTruthy();
     expect(point <= 700).toBeTruthy();
   });
 
   it('포인트 지급은 0~2000원 사이이다.', () => {
     const sut = new RandomPoint();
 
-    const point = sut.generateRandomPointForFirst();
+    const point = sut.generateRandomPoint();
 
     expect(point >= 0).toBeTruthy();
     expect(point <= 2000).toBeTruthy();

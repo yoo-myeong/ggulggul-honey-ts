75baa7059d5dc9712105c4c600b01a681ee6cfd1
sceonn <yoou201@naver.com>
2024-01-20 23:47:16 +0900

    feat: add point to rdb worker

--- src/worker/consumer/addPointToRdb/index.ts
+++ src/worker/consumer/addPointToRdb/index.ts
@@ -1,10 +1,13 @@
 import 'reflect-metadata';
 import { SQSHandler, SQSEvent } from 'aws-lambda';
 import { getWorkerContainer } from '../../config/getWorkerContainer';
-import { UserPointRepository } from '../../../libs/repository/userPoint/userPoint.repository';
+import { AddPointToRdbService } from '../../service/addPointToRdb.service';
 
 export const handler: SQSHandler = async (sqsEvent: SQSEvent) => {
-  const { coinId } = JSON.parse(sqsEvent.Records[0].body);
+  const records = sqsEvent.Records;
+  const coinIds: number[] = records.map((e) => JSON.parse(e.body).coinIds);
 
-  const [addPointService] = [getWorkerContainer().get(UserPointRepository)];
+  const [addPointToRdbService] = [getWorkerContainer().get(AddPointToRdbService)];
+
+  await addPointToRdbService.addPointByCoinIds(coinIds);
 };

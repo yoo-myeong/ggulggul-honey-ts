a0a414fe0ca13d7ba4b59ca57eb25487462297db
sceonn <yoou201@naver.com>
2024-01-25 06:47:30 +0900

    chore: remove severless handler

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,6 @@
 /* eslint-disable import/no-unresolved */
 import 'reflect-metadata';
 import { InversifyExpressServer } from 'inversify-express-utils';
-import serverless from 'serverless-http';
 import { getApiContainer } from './config/getApiContainer';
 import { MYSQL, REDIS } from '../libs/config/configContainer';
 import { App } from './App';
@@ -9,13 +8,16 @@ import { App } from './App';
 import './test/test.controller';
 import { TypeOrm } from '../libs/repository/TypeOrm';
 import { IoRedis } from '../libs/redis/IoRedis';
+import { Config } from '../libs/config/config';
 
-export const handler = async (event: never, context: never) => {
+export const handler = async () => {
   await TypeOrm.connect(MYSQL);
   IoRedis.connect(REDIS);
 
   const server = new InversifyExpressServer(getApiContainer());
   const { app } = new App(server);
 
-  return serverless(app)(event, context);
+  app.listen(Config.cast('APP_PORT').getParsedInt());
 };
+
+handler();
